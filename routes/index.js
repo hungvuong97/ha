72976c6d57141ab522fa56a9d9a15fbb23b78f40
thangnx <thangnx97@gmail.com>
2023-11-29 22:43:16 +0700

    [update]

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require("bcryptjs");
 
 var database = require('../database');
 
@@ -20,13 +21,15 @@ router.post('/login', function(request, response, next){
         WHERE user_email = "${user_email_address}"
         `;
 
-        database.query(query, function(error, data){
+        database.query(query, async function(error, data){
 
             if(data.length > 0)
             {
                 for(var count = 0; count < data.length; count++)
                 {
-                    if(data[count].user_password == user_password)
+                    const passwordMatch = await bcrypt.compare(user_password,data[count].user_password);
+
+                    if(passwordMatch)
                     {
                         request.session.user_id = data[count].user_id;
 
@@ -67,28 +70,63 @@ router.post('/register',async function(request, response, next){
     })
 
     const hashedPassword = await bcrypt.hash(user_password, 10);
-    query = `INSERT INTO users (user_email, user_password) VALUES (${user_email_address}, ${hashedPassword})`;
+    query = `INSERT INTO user_login (user_email, user_password, user_session_id) VALUES ("${user_email_address}", "${hashedPassword}", '')`;
 
     database.query(query, function(error, data){
+        console.log(error)
         if(!error){
-            return res.status(201).json({ message: 'Create user successfully' });
+            response.redirect("/");
         }else{
-            res.status(500).json({ message: 'Server error' });
+            response.status(500).json({ message: 'Server error' });
         }
     })
-
-
-    
-
 })
 
 router.get('/logout', function(request, response, next){
 
     request.session.destroy();
-
     response.redirect("/");
 
 });
+
+router.get('/register', function(request, response, next){
+
+    request.session.destroy();
+    response.render("register");
+
+});
+
+router.get('/memory', function(request, response, next){
+    response.render("memory");
+});
+
+
+
+router.post('/memory',async function(request, response, next){
+    var user_email_address = request.body.user_email_address;
+    var user_password = request.body.user_password;
+    var query = `
+        SELECT * FROM user_login 
+        WHERE user_email = "${user_email_address}"`;
+    database.query(query, function(error, data){
+        if(data.length > 0){
+            return res.status(400).json({ message: 'User existed' });
+        }
+    })
+
+    const hashedPassword = await bcrypt.hash(user_password, 10);
+    query = `INSERT INTO user_login (user_email, user_password, user_session_id) VALUES ("${user_email_address}", "${hashedPassword}", '')`;
+
+    database.query(query, function(error, data){
+        console.log(error)
+        if(!error){
+            response.redirect("/");
+        }else{
+            response.status(500).json({ message: 'Server error' });
+        }
+    })
+})
+
 
 module.exports = router;
 
